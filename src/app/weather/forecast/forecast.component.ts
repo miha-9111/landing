5904620a2e66e7f93673599566c2c68d5ad8eb4a
miha-9111 <miha-9111@yandex.ru>
2022-02-08 23:03:00 +0300

    Accessing Data in the Template

--- src/app/weather/forecast/forecast.component.ts
+++ src/app/weather/forecast/forecast.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ForecastService } from "../forecast.service";
 
+interface ForecastData {
+  dateString: string;
+  temp: string;
+}
+
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
   styleUrls: ['./forecast.component.scss']
 })
 export class ForecastComponent implements OnInit {
+  forecastData: ForecastData[] = [];
 
   constructor(private forecastService: ForecastService) {}
 
   ngOnInit() {
-    this.forecastService.getForecast().subscribe(weatherResponse => {
-        console.log(weatherResponse);
+    this.forecastService.getForecast().subscribe(forecastData => {
+        console.log(forecastData);
+        this.forecastData = forecastData;
     });
   }
 }
