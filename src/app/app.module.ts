995ed454b668274b3f212c9b6bce6e20c17594dd
miha-9111 <miha-9111@yandex.ru>
2022-02-09 02:28:00 +0300

    Generating the Notification Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { WeatherModule } from "./weather/weather.module";
+import { NotificationsModule } from "./notifications/notifications.module";
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    WeatherModule
+    WeatherModule,
+    NotificationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
